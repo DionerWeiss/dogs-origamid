20fa163a099b88ad5e42c5468457db4b3f131766
Dioner <dionerweiss@gmail.com>
2021-11-03 22:47:37 -0300

    feed modal part 1

--- src/components/Feed/FeedPhotos.js
+++ src/components/Feed/FeedPhotos.js
@@ -7,7 +7,7 @@ import FeedPhotosItem from './FeedPhotosItem'
 
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
   const { data, loading, error, request } = useFetch()
 
@@ -24,7 +24,13 @@ const FeedPhotos = () => {
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo}/>)}
+        {data.map(photo => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
       </ul>
     )
   else return null
