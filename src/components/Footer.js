96d0e609518eb83a3d2f6b3b7e8124bf19f9e6ce
Dioner <dionerweiss@gmail.com>
2021-11-17 19:55:49 -0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import styles from './Footer.module.css'
 
+import { ReactComponent as Dogs } from '../assets/dogs-footer.svg'
+
 function Footer() {
-  return <div />;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservador</p>
+    </footer>
+  );
 }
 
 export default Footer;
